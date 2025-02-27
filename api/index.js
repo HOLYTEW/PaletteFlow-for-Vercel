009b26f42625b865c65361a4bfffa4c30775172d
Turian <151127900+HOLYTEW@users.noreply.github.com>
2025-02-28 05:11:33 +0700

    Fix vercel not found issue

--- api/index.js
+++ api/index.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// Define routes for additional pages
 app.get('/tool', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'tool.html'));
+    res.sendFile(path.join(__dirname, '../views/tool.html'));
 });
 
 app.get('/palette', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'palette.html'));
+    res.sendFile(path.join(__dirname, '../views/palette.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+    res.sendFile(path.join(__dirname, '../views/about.html'));
 });
 
-app.use((req, res, next) => {
+// Handle 404 errors
+app.use((req, res) => {
     res.status(404).send('Sorry, that page does not exist!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Export the Express app for Vercel's serverless function
+module.exports = app;
